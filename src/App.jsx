73d8201d826ee060a0f4a00cd0b3d3ec34958771
CosmeNicolas <nicomas7590@gmail.com>
2024-01-30 10:06:09 -0300

    inicialice el proyecto de rutas con react.router dom

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import {Container} from 'react-bootstrap'
+
+function App() {
+
+
+  return (
+   <>
+  <Container>
+    <h1 className='text-light'>Rutas</h1>
+  </Container>
+   </>
+  )
+}
+
+export default App
