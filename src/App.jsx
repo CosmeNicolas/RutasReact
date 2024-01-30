d00cc6d3923c6dd3e8b91c38e016f26d963496c4
CosmeNicolas <nicomas7590@gmail.com>
2024-01-30 10:53:26 -0300

    cree el componente padre de renderizado y luego le agregue las rutas que voy a nevegar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
-import {Container} from 'react-bootstrap'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import HomeScreen from './pages/HomeScreen';
+import AboutScreen from './pages/About';
+/* import {Container} from 'react-bootstrap' */
 
 function App() {
 
 
   return (
-   <>
-  <Container>
-    <h1 className='text-light'>Rutas</h1>
-  </Container>
-   </>
-  )
+    <>
+      <BrowserRouter>
+        <Routes>
+          {/* primera pagina de renderizado por eso colocamos la barra con el props path; q es la direccion de la ruta y el componente elemento que es el componete al q nos dirigimos  */}
+          <Route path='/' element={<HomeScreen/>} />
+          {/* Agregamos otra Ruta de la cual quiero navehar */}
+          <Route path='about' element={<AboutScreen/>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App
---
+++ src/pages/ErrorScren.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ErrorScren = () => {
+  return (
+    <div>ErrorScren</div>
+  )
+}
+
+export default ErrorScren
